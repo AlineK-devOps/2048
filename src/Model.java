f6bfbd6c76786a02b491c00a01abf71b459d965a
Aline Korshunova <aline.korshunova00@gmail.com>
2021-06-21 14:33:38 +0300

    Filling the playing field with empty cells

--- src/Model.java
+++ src/Model.java
@@ -1,2 +1,15 @@
 public class Model { //содержит игровую логику и хранит игровое поле
+    private static final int FIELD_WIDTH = 4; //ширина игрового поля
+
+    private Tile[][] gameTiles; //игровое поле
+
+    public Model() {
+        gameTiles = new Tile[FIELD_WIDTH][FIELD_WIDTH]; //заполнение поля пустыми ячейками
+
+        for (int i = 0; i < FIELD_WIDTH; i++){
+            for (int j = 0; j < FIELD_WIDTH; j++){
+                gameTiles[i][j] = new Tile();
+            }
+        }
+    }
 }
